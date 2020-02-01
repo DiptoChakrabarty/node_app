e5555da4e5eae0141b48aa7c9601ba03b9770d8c
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-01 09:05:04 +0530

    cluster setup

--- kube/scale.sh
+++ kube/scale.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 kubectl apply -f kube
 kubectl scale --replicas=2 deployment/blog
-kubectl get pods -l app=knote --watch
+kubectl get pods -l app=blog --watch
+minikube service blog
+
+
+
+# List of Commands
+Master Node
+kubeadm init --apiserver-advertise-address $(hostname -i)
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+
+# ADD USER
+useradd dipto -G sudo -m -s /bin/bash
+passwd dipto
+
+cd $HOME
