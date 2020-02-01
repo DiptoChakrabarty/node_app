79520e0c326328d8f6a5263b413448347917c919
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-01 09:42:39 +0530

    token in master

--- kube/scale.sh
+++ kube/scale.sh
@@ -23,3 +23,11 @@ sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 echo "export KUBECONFIG=$HOME/admin.conf" | tee -a ~/.bashrc
+
+# Check master node status
+kubectl get pods -n kube-system
+
+
+# Save Token 
+kubeadm token create --print-join-command
+kubeadm join 192.168.0.18:6443 --token rppnz7.rpsfyr44zh6ookc4     --discovery-token-ca-cert-hash sha256:00414fa1eab8fe5784f4d03baa5a1a13139f8cde6f6b1d5610ce92e713048600
