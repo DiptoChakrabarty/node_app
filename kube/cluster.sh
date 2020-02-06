bb79de08655982a180560c8d8b00cc34d665526a
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2020-02-06 11:48:31 +0530

    Run this to configure

--- kube/cluster.sh
+++ kube/cluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+echo "export KUBECONFIG=$HOME/admin.conf" | tee -a ~/.bashrc
